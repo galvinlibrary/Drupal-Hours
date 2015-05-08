23ab9ebf08d70078d454781ac9afc89f1c4ff668
Christine McClure <camcclure@gmail.com>
2015-05-08 10:13:36 -0500

    added test calendar so can test hours easily

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
 
   include 'functions.php';
-  display_google_calendar_hours("iit.edu_8l0d8qd4qtfn7skmgkiu55uv58%40group.calendar.google.com",1);
+  
+  $galvinCal="iit.edu_8l0d8qd4qtfn7skmgkiu55uv58%40group.calendar.google.com";
+  $testCal='pib8627t3i3hj2bssgorkam67g@group.calendar.google.com';
+  display_google_calendar_hours($testCal,1);
 
 ?>
 
